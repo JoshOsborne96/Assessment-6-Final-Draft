bdb638cf98efbfcc2775e957b4fdfac58b5626ce
Josh Osborne <Josh.Osborne@devmounta.in>
2022-09-14 23:40:47 -0600

    first commit

---
+++ functions.test.ts
@@ -0,0 +1,14 @@
+const {shuffleArray} = require('./utils')
+
+
+// describe('shuffleArray should', () => {
+//     test('return an array', () => {
+
+//         expect().toBe(11)
+//     })
+    
+//     test('', () => {
+//         expect(shuffleArray(Array)).toContain('Rusty')
+//     })
+    
+// })
